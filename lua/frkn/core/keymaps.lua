6c058d711332ceddc25a4bf804c850010018da7e
Furkan <furkan.uelker@3imedia.de>
2023-01-16 14:16:21 +0100

    Added new keymaps

--- lua/frkn/core/keymaps.lua
+++ lua/frkn/core/keymaps.lua
@@ -31,6 +31,8 @@ keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
 keymap.set("n", "<leader>tn", ":tabn<CR>") --  go to next tab
 keymap.set("n", "<leader>tp", ":tabp<CR>") --  go to previous tab
 
+keymap.set("n", "<leader>pp", ":e#<CR>") -- open previous buffer
+keymap.set("n", "<leader>p", ":b#<CR>") -- open previous buffer
 ----------------------
 -- Plugin Keybinds
 ----------------------
