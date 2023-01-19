2253c7c99745dde1a466b8dc61d60dd19de6ec05
Furkan <furkan.uelker@3imedia.de>
2023-01-19 14:10:39 +0100

    Remove format on save

--- lua/frkn/plugins/lsp/null-ls.lua
+++ lua/frkn/plugins/lsp/null-ls.lua
@@ -29,20 +29,11 @@ null_ls.setup({
   -- configure format on save
   on_attach = function(current_client, bufnr)
     if current_client.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            filter = function(client)
-              --  only use null-ls for formatting instead of lsp server
-              return client.name == "null-ls"
-            end,
-            bufnr = bufnr,
-          })
-        end,
-      })
+      vim.keymap.set("n", "<Leader>fm", function()
+      vim.lsp.buf.format({ bufnr = vim.api.nvim_get_current_buf() })
+      end, { buffer = bufnr, desc = "[lsp] format" })
+
+      
     end
   end,
 })
