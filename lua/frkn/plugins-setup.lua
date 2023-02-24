47c2e3085d7c631083df4ad69f60fb506e07f4d5
frknue <furkan.uelker@hotmail.com>
2023-02-25 00:01:55 +0100

    color changes

--- lua/frkn/plugins-setup.lua
+++ lua/frkn/plugins-setup.lua
@@ -34,6 +34,9 @@ return packer.startup(function(use)
   use("nvim-lua/plenary.nvim") -- lua functions that many plugins use
 
   use({ "ellisonleao/gruvbox.nvim" })
+  require("gruvbox").setup({
+    transparent_mode = true,
+  })
 
   use("christoomey/vim-tmux-navigator")
 
