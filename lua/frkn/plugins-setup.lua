0830520a3c1801dbbe5bd38df34abf845f287404
frknue <furkan.uelker@hotmail.com>
2023-04-03 23:03:29 +0200

    added lightspeed.nvim

--- lua/frkn/plugins-setup.lua
+++ lua/frkn/plugins-setup.lua
@@ -129,6 +129,9 @@ return packer.startup(function(use)
   -- git integration
   use("lewis6991/gitsigns.nvim") -- show line modifications on left hand side
 
+  -- LightSpeed
+  use("ggandor/lightspeed.nvim")
+
   if packer_bootstrap then
     require("packer").sync()
   end
